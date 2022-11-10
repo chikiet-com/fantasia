a0119dcf9e2f3bb8971701db7d20963907253e64
chikiet88 <chikiet88@gmail.com>
2022-11-10 17:04:38 +0700

    abc

--- apps/app/src/app/app.module.ts
+++ apps/app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialExampleModule } from './material.module';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, BrowserAnimationsModule,MaterialExampleModule],
   providers: [],
   bootstrap: [AppComponent],
 })
